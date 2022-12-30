5b2aab51a9174f2804381a3bfedfa3fc501dc59c
Q-engineering <info@qengineering.eu>
2022-12-30 15:21:58 +0100

    C++ source code

---
+++ main.cpp
@@ -0,0 +1,58 @@
+// Tencent is pleased to support the open source community by making ncnn available.
+//
+// Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+// modified 12-31-2021 Q-engineering
+
+#include "layer.h"
+#include "net.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+#include "face.h"
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
+        return -1;
+    }
+
+    Face face_detector;
+    face_detector.load("./yolov5-blazeface.param", "./yolov5-blazeface.bin");
+
+    const char* imagepath = argv[1];
+
+    cv::Mat m = cv::imread(imagepath, 1);
+    if (m.empty())
+    {
+        fprintf(stderr, "cv::imread %s failed\n", imagepath);
+        return -1;
+    }
+
+    std::vector<Object> objects;
+    face_detector.detect(m, objects, 640);    //640 is target size. The larger the size, the slower the routine (of course)
+    face_detector.draw_objects(m, objects);
+
+    cv::imshow("RPi4 - 1.95 GHz - 2 GB ram",m);
+    cv::imwrite("test.jpg",m);
+    cv::waitKey();
+
+    return 0;
+}
